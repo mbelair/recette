ea6cd66d5e8367e9cc327f8db49904cf67b96bac
Maxime Belair <belairmaxime1@gmail.com>
2024-04-15 11:28:35 -0400

    dynamic sidenav

--- recette/src/app/app.component.ts
+++ recette/src/app/app.component.ts
@@ -1,17 +1,28 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { Observable, map } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, MatSidenavModule, MatListModule],
+  imports: [CommonModule, RouterOutlet, RouterModule, MatSidenavModule, MatListModule, MatIconModule, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'recette';
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 }
